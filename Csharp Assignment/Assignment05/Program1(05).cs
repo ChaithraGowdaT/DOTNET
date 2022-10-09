cf0041ee19620763e7ac3837217efd5190c5c67a
Chaithra Thirumala <chaithrat@infinite.com>
2022-10-09 10:47:43 +0530

    csharp

---
+++ Csharp Assignment/Assignment05/Program1(05).cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// Create an Abstract class Student with Name, StudentId, Grade as members and also an abstract method Boolean Ispassed(grade) 
+/// which takes grade as an input and checks whether student passed the course or not. 
+/// Create 2 Sub classes Undergraduate and Graduate that inherits all members of the student and overrides Ispassed() method
+/// For the UnderGrad class, if the grade is above 70.0, then isPassed returns true, otherwise it returns false. For the Grad class,
+/// if the grade is above 80.0, then isPassed returns true, otherwise returns false.
+
+/// </summary>
+
+namespace Program1_05_
+{
+    abstract class Student
+    {
+
+        public string Name;
+        public int StudentId;
+        public double Grade;
+
+        abstract public bool  Ispaased(double Grade);
+
+    }
+    class Undergraduate:Student
+    {   
+        public override bool Ispaased(double Grade)
+        {
+
+            this.Grade = Grade;
+
+            if (Grade > 70.0)
+                {
+                    Console.WriteLine("student is passed");
+                    return true;
+                }
+
+                else
+                {
+                    Console.WriteLine("student is failed ");
+                    return true;
+                }
+            
+
+        }
+      
+
+    }
+    class Graduate : Student
+    {     
+        public override bool Ispaased(double Grade)
+        {
+            this.Grade = Grade;
+            if (Grade > 80.0)
+            {
+                Console.WriteLine("student is passed");
+                return true;
+            }
+
+            else
+            {
+                Console.WriteLine("student is failed ");
+                return true;
+            }
+        }        
+    }
+    class Driver
+    { 
+        static void Main(string[] args)
+        {
+            Student student = new Undergraduate();
+            Console.WriteLine(  "Enter youe Nmae:");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Enter student Id: ");
+            int StudentId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Grade : ");
+            double grd = Convert.ToDouble(Console.ReadLine());
+            student.Ispaased(grd);
+            Console.Read();
+            
+
+        }
+    }
+}
---
+++ Csharp Assignment/Assignment05/Program4(05).cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 4. Create an Interface IStudent with int StudentId and string Name as Properties, void ShowDetails() as its method.
+/// Create 2 classes Dayscholar and Resident that implements the interface Properties and Methods.ShowDetails() should show DaysScholar details
+///  and Resident Details accordingly.
+/// </summary>
+
+namespace Program4_05_
+{
+    interface IStudent
+    {
+        int StudentId { get; set; }
+        string Name { get; set; }
+
+       
+
+    }
+
+    class Dayscholar : IStudent
+    {
+        public int Age { get; set; }
+
+        public int Stuid1;
+        public string nme1;
+        public int StudentId
+        {
+            get { return Stuid1; }
+            set { Stuid1 = value; }
+        }
+        public string Name
+        {
+            get { return nme1; }
+            set { nme1 = value; }
+        }
+         public void ShowDetails()
+        {
+            Console.WriteLine("{0} student id and name is {1} of age {2} is Dayscholar",Stuid1,nme1,Age);
+        }
+    }
+    class Resident:IStudent
+    {
+        public int Age2 { get; set; }
+
+        public int Stuid2;
+        public string nme2;
+        public int StudentId
+        {
+            get { return Stuid2; }
+            set { Stuid2 = value; }
+        }
+        public string Name
+        {
+            get { return nme2; }
+            set { nme2 = value; }
+        }
+        public void ShowDetails()
+        {
+            Console.WriteLine("{0} student id and name is {1} of age {2} is Resident", Stuid2, nme2, Age2);
+        }
+    }
+
+
+
+    class IDriverclass
+    {
+        static void Main(string[] args)
+        {
+            Dayscholar dayscholar = new Dayscholar();
+            dayscholar.nme1 = "chaithra";
+            dayscholar.Stuid1 = 20345;
+            dayscholar.Age = 25;
+            dayscholar.ShowDetails();
+            Resident resident = new Resident();
+            resident.nme2 = "raghu";
+            resident.Stuid2 = 4567;
+            resident.Age2 = 32;
+            resident.ShowDetails();
+            Console.Read();
+        }
+
+    }
+}
